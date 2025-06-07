401b0fd82c4f8a0a0cffc09192b8b1935ff817db
Aditya <aditya.2428cseai@gmail.com>
2025-06-07 14:42:26 +0530

    Setup repo to track backend and frontend folders

--- src/middlewares/roleMiddleware.js
+++
@@ -1,6 +0,0 @@
-export const requireAdmin = (req, res, next) => {
-    if (req.user.role !== "ADMIN") {
-        return res.status(403).json({ message: "Access denied. Admins only." });
-    }
-    next();
-};
